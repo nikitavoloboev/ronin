365f8a89ecf703d5b17d0258d23696d0fb38cd18
Nikita <github@nikiv.dev>
2024-12-16 12:28:27 +0100

    use query

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -1,6 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { $simpleGetOfRoninData } from "../actions/actions"
+import { useSuspenseQuery } from "@tanstack/react-query"
 
 function RouteComponent() {
+  const { data } = useSuspenseQuery({
+    queryKey: ["/"],
+    queryFn: () => $simpleGetOfRoninData(),
+  })
+  console.log(data, "data")
   return <></>
 }
 
